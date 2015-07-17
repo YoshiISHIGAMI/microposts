d02197add1b4065d31cbe52ebcbad76104b01985
Yoshi Ishigami <yoshi@ibf.sc>
2015-07-17 18:57:51 +0900

    add update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,10 +20,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile, :country)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
