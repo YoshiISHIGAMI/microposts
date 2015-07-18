ad6f1b8ab1ce79bb31857c30920f3e99d71ba2b5
Yoshi Ishigami <yoshi@ibf.sc>
2015-07-18 22:26:08 +0900

    add home view's timeline

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
-
   def home
     #@user = current_user.find(params[:id])
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    end
   end
 end
