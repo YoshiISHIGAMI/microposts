3540f1b577380799aba640802027f9cac7eb02cb
Yoshi Ishigami <yoshi@ibf.sc>
2015-07-18 17:24:39 +0900

    add post function but no flash message

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+
   def home
+    #@user = current_user.find(params[:id])
+    @micropost = current_user.microposts.build if logged_in?
   end
 end
